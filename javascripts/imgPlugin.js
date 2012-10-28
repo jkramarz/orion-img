289cf8f9a36b0b29ba73ca51d998cd5bcbd25479
jkramarz <lenwe@lenwe.net>
2012-10-28 09:11:19 -0400

    License info added

--- javascripts/imgPlugin.js
+++ javascripts/imgPlugin.js
@@ -1,4 +1,13 @@
-/*global window orion */
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2012 Jakub Kramarz
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: Jakub Kramarz
+ ******************************************************************************/
 
 window.onload = function() {
 
